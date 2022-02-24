a74ca76da859007f140a47f308d3ff5bd8c0d760
Jai A P <78354625+Jai-JAP@users.noreply.github.com>
2022-02-24 17:29:31 +0530

    Update post_install.py

--- post_install.py
+++ post_install.py
@@ -48,7 +48,7 @@ with open(os.environ['MESON_BUILD_ROOT'] + '/config.h') as conf:
         if 'pikiss-script' in line:
             fields = line.strip().split()
             pikiss = fields[2]
-        if 'PIKISSDIR' in line:
+        if 'pikissdir' in line:
             fields = line.strip().split()
             pikissdir = fields[2]
 
