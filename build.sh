b189ad0c233604c0749301716c2be257009c594a
Jai A P <jai.jap.318@gmail.com>
2022-04-12 19:04:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf ./build 
-meson --prefix=${HOME}/.local --pikissdir=${HOME}/piKiss -Dbuildtype=release build
+meson --prefix="${HOME}/.local" --pikissdir="${HOME}/piKiss" -Dbuildtype=release build
 ninja -C build -j4
 sudo ninja -C build install
 sudo ninja -C build postinst
