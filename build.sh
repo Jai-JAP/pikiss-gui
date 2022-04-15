4da280f170426a753328add108ce8f0fc7b504ee
Jai A P <jai.jap.318@gmail.com>
2022-04-15 14:31:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 rm -rf ./build 
-meson --prefix="${HOME}/.local" -Dpikissdir="${HOME}/piKiss" -Dbuildtype=release build
+if [[ -n $PIKISSDIR" ]]; then
+  meson --prefix="${HOME}/.local" -Dpikissdir="${PIKISSDIR}" -Dbuildtype=release build
+else 
+  meson --prefix="${HOME}/.local" -Dpikissdir="${HOME}/piKiss" -Dbuildtype=release build
+fi
 ninja -C build -j4
 sudo ninja -C build install
 sudo ninja -C build postinst
