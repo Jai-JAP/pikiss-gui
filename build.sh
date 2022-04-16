e8b721d2a01c7049c2f2ef7079ce5ddb467bc5c2
Jai A P <jai.jap.318@gmail.com>
2022-04-16 10:49:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf ./build 
-if [[ -n $PIKISSDIR" ]]; then
+if [[ -n "$PIKISSDIR" ]]; then
   meson --prefix="${HOME}/.local" -Dpikissdir="${PIKISSDIR}" -Dbuildtype=release build
 else 
   meson --prefix="${HOME}/.local" -Dpikissdir="${HOME}/piKiss" -Dbuildtype=release build
